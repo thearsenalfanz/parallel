60550ac60eb9bee867b290668336bd49554459a7
Por Bergkamp <sodanamkang@email.com>
2019-03-19 22:41:35 -0500

    no message

--- pp1_min.c
+++ pp1_min.c
@@ -71,14 +71,14 @@ void *find_min_rw(void *list_ptr) {
 		if (partial_list_pointer[i] < my_min)
 			my_min = partial_list_pointer[i];
 	/* lock the mutex associated with minimum_value and update the variable as required */
-	mylib_rwlock_rlock(&read_write_lock);
+	pthread_rwlock_rlock(&read_write_lock);
 	if (my_min < minimum_value) {
-		mylib_rwlock_unlock(&read_write_lock);
-		mylib_rwlock_wlock(&read_write_lock);
+		pthread_rwlock_unlock(&read_write_lock);
+		pthread_rwlock_wlock(&read_write_lock);
 		minimum_value = my_min;
 	}
 	/* and unlock the mutex */
-	mylib_rwlock_unlock(&read_write_lock);
+	pthread_rwlock_unlock(&read_write_lock);
 	pthread_exit(0);
 }
 
