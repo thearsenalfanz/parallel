0b0ea541b9bc685541c147c8cf62f8d21f02d5c8
Por Bergkamp <sodanamkang@email.com>
2019-03-19 23:02:59 -0500

    no message

--- pp1_min.c
+++ pp1_min.c
@@ -85,7 +85,37 @@ void mylib_rwlock_rlock(mylib_rwlock_t *l) {
 	pthread_mutex_unlock(&(l -> read_write_lock));
 }
 
+void mylib_rwlock_wlock(mylib_rwlock_t *l) {
+	/* if there are readers or writers, increment pending writers
+	count and wait. On being woken, decrement pending writers
+	count and increment writer count */
+	pthread_mutex_lock(&(l -> read_write_lock));
+	while ((l -> writer > 0) || (l -> readers > 0)) {
+		l -> pending_writers ++;
+		pthread_cond_wait(&(l -> writer_proceed),
+			&(l -> read_write_lock));
+	}
+	l -> pending_writers --;
+	l -> writer ++
+	pthread_mutex_unlock(&(l -> read_write_lock));
+}
 
+void mylib_rwlock_unlock(mylib_rwlock_t *l) {
+	/* if there is a write lock then unlock, else if there are
+	read locks, decrement count of read locks. If the count
+	is 0 and there is a pending writer, let it through, else
+	if there are pending readers, let them all go through */
+	pthread_mutex_lock(&(l -> read_write_lock));
+	if (l -> writer > 0)
+		l -> writer = 0;
+	else if (l -> readers > 0)
+		l -> readers --;
+	pthread_mutex_unlock(&(l -> read_write_lock));
+	if ((l -> readers == 0) && (l -> pending_writers > 0))
+		pthread_cond_signal(&(l -> writer_proceed));
+	else if (l -> readers > 0)
+		pthread_cond_broadcast(&(l -> readers_proceed));
+}
 
 
 /* ---------------------------------------- FIND_MIN_RW */
