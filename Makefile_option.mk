4124a6409cef9587ac707b89d89a9d1b4a624123
Por Bergkamp <sodanamkang@email.com>
2019-03-20 20:42:40 -0500

    no message

--- Makefile_option.mk
+++ Makefile_option.mk
@@ -3,12 +3,15 @@ LIB = -lpthread
 EXE0 = pp1_min_op
 
 $(EXE0): $(EXE0).exe
-	./$<	
+	./$<	-T 1
+	./$<	-T 2
+	./$<	-T 4
+	./$<	-T 8
 
 %.exe : %.c
 	$(CC) $(LIB) -o $@ $^
 
-all: $(EXE0) -T 1 $(EXE0) -T 2 $(EXE0) -T 4 $(EXE0) -T 8
+all: $(EXE0)
 
 clean:
 	rm -f *.exe 
