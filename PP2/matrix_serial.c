003a5b816c0118d13ba74742348b18070759e86b
thearsenalfanz <sodanamkang@email.com>
2019-03-25 22:44:43 -0500

    no message

---
+++ PP2/matrix_serial.c
@@ -0,0 +1,112 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+#include <sys/time.h>
+
+/* -----------------------------------Global Vars */
+#define MAXN 100
+int tid, nthreads, N;
+volatile float A[MAXN][MAXN], B[MAXN][MAXN], C[MAXN][MAXN];
+
+/* Initialize A and B (and X to 0.0s) */
+void initialize_inputs() {
+    int row, col;
+
+    printf("\nInitializing inputs\n");
+    for (col = 0; col < N; col++) {
+        for (row = 0; row < N; row++) {
+            A[row][col] = (float)rand() / 32768.0;
+            B[row][col] = (float)rand() / 32768.0;
+        }
+    }
+
+}
+
+
+/* Print input matrices */
+void print_inputs() {
+    int row, col;
+
+    if (N < 10) {
+        printf("\nA =\n\t");
+        for (row = 0; row < N; row++) {
+            for (col = 0; col < N; col++) {
+                printf("%5.2f%s", A[row][col], (col < N-1) ? ", " : ";\n\t");
+            }
+        }
+        printf("\nB =\n\t");
+        for (row = 0; row < N; row++) {
+            for (col = 0; col < N; col++) {
+                printf("%5.2f%s", B[row][col], (col < N-1) ? ", " : ";\n\t");
+            }
+        }
+    }
+}
+
+
+void print_output() {
+    int row, col;
+
+    if (N < 10) {
+        printf("\nC =\n\t");
+        for (row = 0; row < N; row++) {
+            for (col = 0; col < N; col++) {
+                printf("%5.2f%s", C[row][col], (col < N-1) ? ", " : ";\n\t");
+            }
+        }
+    }
+}
+
+/* ---------------------------------------- MYSECOND */
+static double 
+gettime()
+{
+    struct timeval  tp;
+    struct timezone tzp;
+    int i = 0;
+
+    i = gettimeofday(&tp, &tzp);
+    return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+}
+
+int main(int argc, char **argv)
+{
+    int i,j,k,dim;
+    double start,end;
+
+    nthreads = 4;
+    N = 4;
+
+    initialize_inputs();
+
+    print_inputs();
+
+    printf("Before set: %d\n", omp_get_num_threads());
+    omp_set_num_threads(4);
+    printf("Set: %d\n", omp_get_num_threads());
+
+    dim = N;
+
+    start = gettime();
+
+    for (i = 0; i < dim; i++) {
+        tid = omp_get_thread_num();
+        printf("Hello World from thread = %d\n", tid);
+        for (j = 0; j < dim; j++) {
+            C[i][j] = 0;
+            for (k = 0; k < dim; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    end = gettime();
+
+    print_output();
+    printf("\n");
+    printf("Runtime of %d threads = %f seconds\n", nthreads, (end-start));
+    printf("\n");
+
+
+    exit(0);
+}
