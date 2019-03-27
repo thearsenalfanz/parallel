bfdc0ab5ca25668b2e42bd903519b65ef6310734
thearsenalfanz <sodanamkang@email.com>
2019-03-27 14:28:49 -0500

    no message

---
+++ PP2/Q1/test2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NAME=matrix_2inner_test
+C="$NAME.c"
+EXE="$NAME.exe"
+gcc "$C" -o "$EXE" -fopenmp -w
+for j in 1 2 4 8
+do
+	for((i=10; i< 100; i+=10))  
+	do
+		./"$EXE" -N $i -T $j
+    done
+	for((i=100; i< 1500; i+=100))  
+	do
+		./"$EXE" -N $i -T $j
+    done
+done
+make clean
