7317743b8bb3553e0a0a5989e2c6fff0b0895770
thearsenalfanz <sodanamkang@email.com>
2019-03-27 12:03:46 -0500

    no message

--- PP2/Q2/gauss.c
+++ PP2/Q2/gauss.c
@@ -223,7 +223,7 @@ void gauss() {
 
   /* Gaussian elimination */
   for (norm = 0; norm < N - 1; norm++) {
-    #pragma omp parallel for private(multiplier, row, col) shared (A, B) //num_threads(procs)
+    #pragma omp parallel for private(multiplier, row, col) shared (A, B)
     for (row = norm + 1; row < N; row++) {
       // tid = omp_get_thread_num();
       // printf("Hello World from thread = %d\n", tid);
