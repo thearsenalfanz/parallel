a5f7c303e1df7efbd081184a2c631ef30970b9df
Por Bergkamp <sodanamkang@email.com>
2019-03-27 07:18:51 -0500

    no message

---
+++ PP2/Q1/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+gcc matrix_print.c -o ./matrix_print.exe -fomp -w
+for j in 1 2 4 8
+do
+	for((i=0; i< 10; i+=1))  
+	do
+		./matrix_print.exe $i $j
+    done
+done
+do
+	for((i=10; i< 100; i+=10))  
+	do
+		./matrix_print.exe $i $j
+    done
+done
+do
+	for((i=100; i< 2000; i+=100))  
+	do
+		./matrix_print.exe $i $j
+    done
+done
+make clean
---
+++ PP2/Q2/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gcc gauss_test.c -o ./gauss_test.exe -lpthread -w
+for j in 1 2 4 8
+do
+	for((i=100; i< 2000; i+=100))  
+	do
+		./gauss_test.exe $i $j
+    done
+done
+make clean
